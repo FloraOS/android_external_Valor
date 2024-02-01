cc2bde2c2e7aacae2ef2177fe7583185cf7c2840
Andrewerr <toddot@protonmail.com>
2024-02-01 14:43:41 +0100

    [valor] [meta] [security] [sepolicy] [config]: update SEPolicy to working version, use IDLE time 5 seconds

--- src/valord/config.h
+++ src/valord/config.h
@@ -4,7 +4,7 @@
 #define DB_MODULO 65535
 #define DB_SIZE 65535
 #define DB_FILE "/system/etc/valor.db"
-#define IDLE_TIME 10
+#define IDLE_TIME 5
 #define MODNAME "valord"
 #define CHECKSUM_BUFSIZE 8192
 #define DEBUG 1

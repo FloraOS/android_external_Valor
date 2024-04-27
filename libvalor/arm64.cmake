4c641c0a7e938456779e475544089495aec8a92c
Anna-Sophie Zaitsewa <toddot@protonmail.com>
2024-04-27 19:36:59 +0200

    [valor] [meta] [FIX]: do not gitignore .cmake files

---
+++ libvalor/arm64.cmake
@@ -0,0 +1,17 @@
+set(ANDROID_NATIVE_API_LEVEL 26)
+set(ANDROID_ABI "arm64-v8a")
+include(${ANDROID_NDK}/build/cmake/android.toolchain.cmake)
+
+add_library(valor_arm64-v8a STATIC
+        ${LIBVALOR_DIR}/src/checksum.c
+        ${LIBVALOR_DIR}/src/db/db.c
+        ${LIBVALOR_DIR}/src/crc32/crc32.c
+        ${LIBVALOR_DIR}/src/crc32/crc_poly.c
+        ${LIBVALOR_DIR}/src/array.c
+        ${LIBVALOR_DIR}/src/hashtable.c
+)
+
+target_include_directories(valor_arm64-v8a PUBLIC
+        $<BUILD_INTERFACE:${LIBVALOR_DIR}/include>
+        $<INSTALL_INTERFACE:include>
+)

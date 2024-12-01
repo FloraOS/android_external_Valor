24ad50ccb31e2086a7ff9d41d653b1fbde71412e
f104a <f104a@f104a.io>
2024-12-01 21:39:14 +0100

    [valor] [meta]: issue warning about using default valor.db
    Change-Id: Ib19c91d39d3c02b3dac57efc6c78cf8d32efeffe

--- Android.mk
+++ Android.mk
@@ -10,12 +10,15 @@ include $(BUILD_PREBUILT)
 
 
 # Database(should be generated in advance or provided by other repo)
+ifneq ($(VALOR_HAS_CUSTOM_DB), true)
+$(warning VALOR_HAS_CUSTOM_DB is not true. Be sure to define/create valor.db in advance)
 include $(CLEAR_VARS)
 LOCAL_MODULE := valor.db
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
+endif
 
 # Include all module makefiles
 include $(call all-makefiles-under, $(LOCAL_PATH))
